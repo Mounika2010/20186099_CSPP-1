fbd1d7ef82608b8583270b437596762875808fac
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-04 18:15:11 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

---
+++ cspp1-practice/m5/p4/square_root_newtonrapson.py
@@ -0,0 +1,26 @@
+'''
+# Write a python program to find the square root of the given number
+# using approximation method
+
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+
+# testcase 2
+# input: 49
+# output: 6.999999999999991
+'''
+def main():
+    '''
+    @author : Mounika2010
+    this prigram prints the cube rooyt using newton raphson method
+    '''
+    x_i = int(input())
+    epsilon = 0.01
+    guess = x_i/2.0
+    while abs(guess*guess - x_i) >= epsilon:
+        guess = guess - (((guess**2) - x_i)/(2*guess))
+    print(str(guess))
+
+if __name__ == "__main__":
+    main()
