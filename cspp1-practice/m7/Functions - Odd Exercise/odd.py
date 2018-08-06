c93a6cc4f542c6f432437573d38f614b0fb7fe08
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-06 20:00:17 +0530

    prints true if odd, false if not

---
+++ cspp1-practice/m7/Functions - Odd Exercise/odd.py
@@ -0,0 +1,32 @@
+'''
+@author : Mounika2010
+# Exercise: odd
+
+# Write a Python function, odd, that takes in one number and returns True
+when the number is odd and False otherwise.
+
+# You should use the % (mod) operator, not if.
+
+# This function takes in one number and returns a boolean.
+'''
+
+def odd(x_input):
+    '''
+    x: int or float.
+
+    returns: True if x is odd, False otherwise
+    '''
+    if x_input%2 != 0:
+        print('True')
+    else:
+        print('False')
+    # Your code here
+def main():
+    '''
+    true if odd, false if not
+    '''
+    data = input()
+    print(odd(int(data)))
+
+if __name__ == "__main__":
+    main()
