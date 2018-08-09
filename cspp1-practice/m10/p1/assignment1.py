4fe5d19c401a13564c84af909ddf6e79ffd25f2b
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-09 17:59:10 +0530

    " -> 10 of 10 passed. pylint: 9.41/10 "

--- cspp1-practice/m10/p1/assignment1.py
+++ cspp1-practice/m10/p1/assignment1.py
@@ -5,20 +5,19 @@ a list of letters, letters_guessed. This function returns a string
 that is comprised of lowercase English letters - all lowercase English letters
 that are not in letters_guessed
 '''
-import string
 
+import string
 def get_available_letters(letters_guessed):
     '''
     :param letters_guessed: list, what letters have been guessed so far
     returns: string, comprised of letters that represents what letters have not
       yet been guessed.
     '''
-    str_one = 'abcdefghijklmnopqrstuvwxyz'
-    str_two = ''
-    print(string.ascii_lowercase)
-    for i in str_one:
+    check_str = string.ascii_lowercase
+    str_two = ""
+    for i in check_str:
         if i not in letters_guessed:
-            str_two = str_two + i
+            str_two += i
     return str_two
 
             
