74a3848f2523cbb1da5ebbd4c4855517eb9aae39
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-11 19:53:47 +0530

    " -> 0 of 9 passed. pylint: -9.38/10 "

--- cspp1-practice/m11/p1/assignment1.py
+++ cspp1-practice/m11/p1/assignment1.py
@@ -1,13 +1,13 @@
 '''
 @author : Mounika2010
-Exercise: Assignment-1
+Exercise: Assignment-1              
 The first step is to implement some code that allows us to calculate
-the score for a single word. The function get_word_score should accept as input
+the score for a single word. The function get_word_score should accept as INPUT
 a string of lowercase letters (a word) and return the integer score for that word,
 using the game's scoring rules.
 '''
 
-def get_word_score(word, n):
+def get_word_score(word, n_inp):
     """
     Returns the score for a word. Assumes the word is a valid word.
 
@@ -16,14 +16,14 @@ def get_word_score(word, n):
     letters are used on the first turn.
 
     Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
-    worth 3, D is worth 2, E is worth 1, and so on (see SCRABBLE_LETTER_VALUES)
+    worth 3, D is worth 2, E is worth 1, and so on (see scrabble_letter_values)
 
     word: string (lowercase letters)
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
     """
 
-    SCRABBLE_LETTER_VALUES = {
+    s= {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10 }
@@ -31,8 +31,8 @@ def get_word_score(word, n):
     score = 0
     length = len(word)
     for key in b:
-        if key in SCRABBLE_LETTER_VALUES:
-            score = score + SCRABBLE_LETTER_VALUES[key]
+        if key in scrabble_letter_values:
+            score = score + scrabble_letter_values[key]
     score = score * length
     if length == 7:
         return score + 50
@@ -43,8 +43,7 @@ def main():
     '''
     Main function for the given problem
     '''
-    data = input()
-    data = data.split()
+
     print(get_word_score(data[0], int(data[1])))
 
 
