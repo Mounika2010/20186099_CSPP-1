4d7bf0b7f6253bd22fed89f7a2ab0df9c0877a32
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-11 12:02:56 +0530

    " -> 0 of 3 passed. pylint: 7.65/10 "

--- cspp1-practice/m12/p1/create_social_network.py
+++ cspp1-practice/m12/p1/create_social_network.py
@@ -34,9 +34,10 @@ def create_social_network(data):
     # remove the pass below and start writing your code
     
     d = {}
-    for i in range(0,len(data),2):
+    for i in range(0,len(str(data)),2):
         if str(data) not in d:
-            d[str(data)[i]] = (data[i+1]).split(",")
+            d[str(data[i])] = (data[i+1]).split(",")
+
     return d
 
 
