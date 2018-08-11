8bf0a9939e47ef243dbe204e58f9f78558637baa
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-11 21:29:44 +0530

    assignment 2

--- cspp1-practice/m11/p2/assignment2.py
+++ cspp1-practice/m11/p2/assignment2.py
@@ -1,15 +1,18 @@
 '''
 @author : Mounika2010
 #Exercise: Assignment-2
-#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
+#Implement the updateHand function. Make sure this function has
+no side effects: i.e., it must not mutate the hand passed in.
+Before pasting your function definition here, be sure you've passed
+the appropriate tests in test_ps4a.py.
 '''
 import copy
-def updateHand(hand, word):
+def update_hand(hand, word):
     """
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
 
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
@@ -17,7 +20,7 @@ def updateHand(hand, word):
     Has no side effects: does not modify hand.
 
     word: string
-    hand: dictionary (string -> int)    
+    hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
 
@@ -27,16 +30,19 @@ def updateHand(hand, word):
     return temp_hand
 
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1 = input()
-	print(updateHand(adict,data1))
-		
+    '''
+    update the hand
+    '''
+    n_inp = input()
+    adict = {}
+    for i in range(int(n_inp)):
+        del i
+        data = input()
+        l_let = data.split()
+        adict[l_let[0]] = int(l_let[1])
+    data1 = input()
+    print(update_hand(adict, data1))
 
 
 if __name__ == "__main__":
-	main()
+    main()
