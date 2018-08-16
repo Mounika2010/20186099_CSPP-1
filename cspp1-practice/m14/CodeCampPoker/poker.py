86f121a9eede37e70eb95eeb9007ed5ea5a19e2a
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-16 12:54:55 +0530

    " -> 0 of 3 passed. pylint: 4.52/10 "

--- cspp1-practice/m14/CodeCampPoker/poker.py
+++ cspp1-practice/m14/CodeCampPoker/poker.py
@@ -14,7 +14,40 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
-    pass
+    i = 0
+    l_2 = []
+    while i < len(hand):
+    	if hand[i][0] == 'T':
+    		k = hand[i][0].replace("T", "10")
+    		l_2.append(int(k))
+    	elif hand[i][0] == 'J':
+    		k = hand[i][0].replace("J", "11")
+    		l_2.append(int(k))
+    	elif hand[i][0] == 'Q':
+    		k = hand[i][0].replace("Q", "12")
+    		l_2.append(int(k))
+    	elif hand[i][0] == 'K':
+    		k = hand[i][0].replace("K", "13")
+    		l_2.append(int(k))
+    	elif hand[i][0] == 'A':
+    		k = hand[i][0].replace("A", "14")
+    	else:
+    		k = (hand[i][0])
+    		l_2.append(int(k))
+    l_3 = sorted(l_2)
+    p_tr = 0
+    k_tr = 1
+    check1 = 1
+    while k_tr < len(l_3) and check1 == 1:
+    	if l_3[k_tr]-l_3[p_tr] == 1:
+    		pass
+    	else:
+    		check1 = 0
+    	k_tr = k_tr + 1
+    	p_tr = p_tr + 1
+    return check1 == 1
+
+
 
 def is_flush(hand):
     '''
@@ -25,7 +58,18 @@ def is_flush(hand):
         Think of an algorithm: given the card suite how to check if it is a flush
         Write the code for it and return True if it is a flush else return False
     '''
-    pass
+    is_flush_ = 1
+    i = 0
+    j = 1
+    while j < len(hand) and is_flush_ == 1:
+    	if hand[i][1] != hand[j][i]:
+    		is_flush_ = 0
+    	else:
+    		is_flush_ = 1
+    	i = i + 1
+    	j = j + 1
+    return is_flush_ == 1
+
 
 def hand_rank(hand):
     '''
@@ -51,7 +95,16 @@ def hand_rank(hand):
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
     # max in poker function uses these return values to select the best hand
-    return 1
+    is_straigh = is_straight(hand)
+    is_flus = is_flush(hand)
+    if is_flus and is_straigh:
+    	return 3
+    elif is_flus:
+    	return 2
+    elif is_straigh:
+    	return 1
+    else:
+    	return 0
 
 def poker(hands):
     '''
