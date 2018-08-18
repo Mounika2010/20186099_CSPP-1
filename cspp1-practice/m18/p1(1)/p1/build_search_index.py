1372588b965c7fc7cadf5ec2519f6f3f97146f25
Mounika2010 <mangalagirimounika20@msitprogram.net>
2018-08-18 13:19:15 +0530

    " -> 0 of 1 passed. pylint: 7.14/10 "

--- cspp1-practice/m18/p1(1)/p1/build_search_index.py
+++ cspp1-practice/m18/p1(1)/p1/build_search_index.py
@@ -67,10 +67,10 @@ def build_search_index(docs):
     word_l = []
     for line in docs:
         word_l.append(word_list(line))
-        for word in word_m:
+        for word in words_m:
             if word not in dictionary.keys():
                 dictionary[word] = [(i, word_l.count(word)) for i,
-                                    word in enumerate(word_l) if word in word_m]
+                                    word in enumerate(word_l) if word in words_m]
     return dictionary
 
 
